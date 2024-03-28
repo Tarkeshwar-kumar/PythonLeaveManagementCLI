180f7f13d9f0c580d2c2d548b34c248917d5a2ca
Tarkeshwar Kumar <tarkeshwar.kumar@hp.com>
2024-03-28 11:35:19 +0530

    Sending request to aws sqs

--- src/utils/unauthorised.py
+++ src/utils/unauthorised.py
@@ -1,4 +1,7 @@
 from clint.textui import puts, colored
+import json
+from aws_services.sqs.sqs import submit_request
+from exceptions.exceptions import SQSSendMessageError
 options = {
     "A": "Raise a request",
     "B": "Leave the application"
@@ -15,4 +18,35 @@ def user_action():
         form = create_form()
 
 def create_form() -> dict:
-    pass
+    email = input(colored.yellow('Enter your email address: '))
+    password = input(colored.yellow('Enter your password: '))
+    first_name = input(colored.yellow('Enter your first name: '))
+    last_name = input(colored.yellow('Enter your last name name: '))
+    position = input(colored.yellow('Enter your position in the organisation: '))
+    flat_number = input(colored.yellow('Enter your flat number: '))
+    sector = input(colored.yellow('Enter your resedential sector: '))
+    city = input(colored.yellow('Enter your city name: '))
+    state = input(colored.yellow('Enter your state name: '))
+    country = input(colored.yellow('Enter your country name: '))
+
+    employee_details = {
+            "email": email,
+            "password": password,
+            "first_name": first_name,
+            "last_name": last_name,
+            "position": position,
+            "flat_number": flat_number,
+            "sector": sector,
+            "city": city,
+            "state": state,
+            "country": country
+        }
+    json_emp_details = json.dumps(employee_details)
+    # submit_request(json_emp_details)
+    try:
+        submit_request(json_emp_details)
+    except SQSSendMessageError as error:
+        puts(colored.red("Your request couldn't be send, check message body"))
+    else:
+        puts(colored.green("Your request have been submitted"))
+
