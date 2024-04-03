cac42644b4a6068b2d21ddf1815ded0615f52ea0
Tarkeshwar Kumar <tarkeshwar.kumar@hp.com>
2024-04-03 09:35:48 +0530

    Fixed bug in factory pattern

--- src/utils/adapter.py
+++ src/utils/adapter.py
@@ -10,44 +10,45 @@ class Employee:
     def __init__(self, email_id) -> None:
         self.email_id = email_id
 
-    def action():
+    def action(self):
         pass
 
 class Developer(Employee):
-    def action():
-        developer_action()
+    def action(self):
+        print(self.email_id)
+        developer_action(self.email_id)
 
 class Admin(Employee):
-    def action():
+    def action(self):
         admin_choose_action()
 
 class Manager(Employee):
-    def action():
+    def action(self):
         manager_action()
 
 class Unauthorised(Employee):
-    def action():
+    def action(self):
         user_action()
 
 class EmployeeFactory(ABC):
     @abstractclassmethod
-    def create_employee():
+    def create_employee(self):
         pass
 
 class ManagerFactory(EmployeeFactory):
-    def create_employee(email_id):
+    def create_employee(self, email_id):
         return Manager(email_id)
 
 class AdminFactory(EmployeeFactory):
-    def create_employee(email_id):
+    def create_employee(self, email_id):
         return Admin(email_id)
 
 class DeveloperFactory(EmployeeFactory):
-    def create_employee(email_id):
+    def create_employee(self, email_id):
         return Developer(email_id)
 
 class UnauthorisedUserFactory(EmployeeFactory):
-    def create_employee(email_id):
+    def create_employee(self, email_id):
         return Unauthorised(email_id)
     
 role_factory_mapping = {
@@ -64,8 +65,9 @@ def choose_features(email, role):
     employee.action()
 
 def display_options(role) -> None:
-    for feature in features[role]:
-        puts(colored.blue(feature+ " -> "+ features[feature]))
+    options = features[role]
+    for feature in options:
+        puts(colored.blue(feature+ " -> "+ options[feature]))
 
 def action(role):
     pass
