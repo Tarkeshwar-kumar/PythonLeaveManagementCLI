053b4e4a8b607fcf105fadd59cdc1536ade16ef4
Tarkeshwar Kumar <tarkeshwar.kumar@hp.com>
2024-04-03 08:27:16 +0530

    Separated display_options from if ladder

--- src/utils/adapter.py
+++ src/utils/adapter.py
@@ -1,24 +1,34 @@
-from utils.features import *
+from utils.features import features
 from utils.admin import admin_choose_action
 from utils.developers import developer_action
 from utils.manager import manager_action
 from utils.unauthorised import user_action
 from clint.textui import puts, colored
+from abc import ABC, abstractclassmethod
 
-def choose_features(email, role= "unauthorised"):
+class Action(ABC):
+    @abstractclassmethod
+    def action():
+        pass
+
+class Manager_action(Action):
+    def action():
+        admin_choose_action()
+
+def choose_features(email, role):
+    display_options(role)
     if role == "Manager":
-        display_options(manager_features)
         manager_action(email)
     elif role == "Developer":
-        display_options(developer_features)
         developer_action(email)
     elif role == "Admin":
-        display_options(admin_features)
-        admin_choose_action()
+        admin_choose_action(email)
     else:
-        display_options(unothorised_features)
         user_action()
 
-def display_options(features) -> None:
-    for option in features:
-        puts(colored.blue(option+ " -> "+ features[option]))
+def display_options(role) -> None:
+    for feature in features[role]:
+        puts(colored.blue(feature+ " -> "+ features[feature]))
+
+def action(role):
+    pass
--- src/utils/features.py
+++ src/utils/features.py
@@ -1,24 +1,23 @@
-admin_features = {
-    "A": "See employee details",
-    "B": "Add employee",
-    "C": "Remove employee"
-}
-
-developer_features = {
-    "A": "See my details",
-    "B": "See leave statistics",
-    "C": "Apply for leaves",
-    "D": "Cancel applied leaves"
-}
-
-manager_features = {
-    "A": "See my details",
-    "B": "See leave statistics",
-    "C": "Approve leaves",
-    "D": "Reject leaves"
-}
-
-unothorised_features = {
-    "A": "Raise a request",
-    "B": "Leave the application"
+features = {
+    "Manager": {
+        "A": "See my details",
+        "B": "See leave statistics",
+        "C": "Approve leaves",
+        "D": "Reject leaves"
+    },
+    "Admin" : {
+        "A": "See employee details",
+        "B": "Add employee",
+        "C": "Remove employee"
+    },
+    "Developer": {
+        "A": "See my details",
+        "B": "See leave statistics",
+        "C": "Apply for leaves",
+        "D": "Cancel applied leaves"
+    },
+    "Unauthorized": {
+        "A": "Raise a request",
+        "B": "Leave the application"
+    }
 }
